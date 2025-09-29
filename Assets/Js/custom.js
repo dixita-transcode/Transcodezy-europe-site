e9ea294ab94a3020a9b261cfdf4ccef96425eeec
kishan <kishan.transcode@gmail.com>
2025-09-29 18:23:42 +0530

    first commit

---
+++ Assets/Js/custom.js
@@ -0,0 +1,34 @@
+// mobile menu
+const menuIcon = document.getElementById("menu-icon");
+const mobMenu = document.querySelector(".mob-menu");
+
+menuIcon.addEventListener("click", () => {
+  menuIcon.classList.toggle("active");
+  mobMenu.classList.toggle("active");
+});
+
+
+
+// menuitem click 
+const menuLinks = document.querySelectorAll('.menu .menu-items li > a');
+
+menuLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    menuLinks.forEach(li => li.classList.remove('active'));
+    link.classList.add('active');
+  });
+});
+
+
+// Toggle submenu in mobile screen
+document.querySelectorAll(".mob-menu_item_link").forEach(btn => {
+  btn.addEventListener("click", () => {
+    const submenu = btn.closest(".mob-menu_item").querySelector(".mob-submenu");
+    submenu.classList.toggle("hidden");
+
+    btn.querySelector("i").classList.toggle("rotate-180");
+  });
+});
+
+
+
---
+++ Assets/Js/tailwind.config.js
@@ -0,0 +1,25 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        primary: '#0094EE', // Primary brand color (logo, buttons, highlights)
+        gradientStart: '#FFFFFF00', // Indigo-600
+        gradientEnd: '#F6F6F7F5',   // Blue-500
+      },
+      container: {
+        center: true,
+        padding: '1rem',
+        screens: {
+          lg: '1280px',
+          xl: '1280px',
+          xxl:"1440px",
+        },
+      },
+      backgroundImage:{
+        'hero-sec':"url('/')"
+      }
+  
+    },
+  },
+  plugins: [],
+}
