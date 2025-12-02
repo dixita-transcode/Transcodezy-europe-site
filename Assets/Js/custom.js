ad59e2f75173adf53b7ed3ee4f19f423cf49964f
kishan <kishan.transcode@gmail.com>
2025-12-02 18:02:37 +0530

    service in ourservice section changes done

--- Assets/Js/custom.js
+++ Assets/Js/custom.js
@@ -10,42 +10,42 @@ menuIcon.addEventListener("click", () => {
 
 
 // menuitem click 
-// const menuLinks = document.querySelectorAll('.menu .menu-items li > a');
+const menuLinks = document.querySelectorAll('.menu .menu-items li > a');
 
-// menuLinks.forEach(link => {
-//   link.addEventListener('click', () => {
-//     menuLinks.forEach(li => li.classList.remove('active'));
-//     link.classList.add('active');
-//   });
-// });
+menuLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    menuLinks.forEach(li => li.classList.remove('active'));
+    link.classList.add('active');
+  });
+});
 
 
 // Toggle submenu in mobile screen
-// document.querySelectorAll(".mob-menu_item_link").forEach(btn => {
-//   btn.addEventListener("click", () => {
-//     const currentItem = btn.closest(".mob-menu_item");
-//     const currentSubmenu = currentItem.querySelector(".mob-submenu");
-//     const currentIcon = btn.querySelector("i");
+document.querySelectorAll(".mob-menu_item_link").forEach(btn => {
+  btn.addEventListener("click", () => {
+    const currentItem = btn.closest(".mob-menu_item");
+    const currentSubmenu = currentItem.querySelector(".mob-submenu");
+    const currentIcon = btn.querySelector("i");
 
-//     // Close all other submenus
-//     document.querySelectorAll(".mob-submenu").forEach(sub => {
-//       if (sub !== currentSubmenu) {
-//         sub.classList.add("hidden");
-//       }
-//     });
+    // Close all other submenus
+    document.querySelectorAll(".mob-submenu").forEach(sub => {
+      if (sub !== currentSubmenu) {
+        sub.classList.add("hidden");
+      }
+    });
 
-//     // Reset all icons
-//     document.querySelectorAll(".mob-menu_item_link i").forEach(icon => {
-//       if (icon !== currentIcon) {
-//         icon.classList.remove("rotate-180");
-//       }
-//     });
+    // Reset all icons
+    document.querySelectorAll(".mob-menu_item_link i").forEach(icon => {
+      if (icon !== currentIcon) {
+        icon.classList.remove("rotate-180");
+      }
+    });
 
-//     // Toggle the current submenu
-//     currentSubmenu.classList.toggle("hidden");
-//     currentIcon.classList.toggle("rotate-180");
-//   });
-// });
+    // Toggle the current submenu
+    currentSubmenu.classList.toggle("hidden");
+    currentIcon.classList.toggle("rotate-180");
+  });
+});
 
 
 
